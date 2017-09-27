8fd6bd69d89c54469d0540583b735587e33d7db2
fent <933490+fent@users.noreply.github.com>
2017-09-27 00:05:22 -0400

    A better example

--- chrome.js
+++ chrome.js
@@ -2,9 +2,21 @@ window.chrome = {
   storage: {
     sync: {
       get: function(keys, callback) {
-        setTimeout(function() { callback({}); });
+        var store = {};
+        keys.forEach(function(key) {
+          try {
+            store[key] = JSON.parse(localStorage.getItem(key));
+          } catch (e) {
+            store[key] = null;
+          }
+        });
+        setTimeout(function() { callback(store); });
       },
-      set: function() {},
+      set: function(values) {
+        for (var key in values) {
+          localStorage.setItem(key, JSON.stringify(values[key]));
+        }
+      },
     },
   },
   runtime: {
--- options.js
+++ options.js
@@ -1,27 +1,47 @@
-chrome.options.opts.about = 'This is my about page :)';
+/* global chrome */
+
+chrome.options.opts.about = `
+  <p>This is my about page :)</p>
+  <p>
+    See how this demo's options are structured
+    in the <a href="options.js">options.js</a> file
+  </p>
+`;
+
 chrome.options.addTab('General', [
   { name: '1', desc: 'Enable my feature' },
-	{ name: '2', desc: 'Enable watching' },
-	{ name: '3', desc: 'Allow users to start it out' },
-	{ type: 'h3', desc: 'Section' },
-	{ name: '4', type: 'object', options: [
-		{ name: 'enabled', type: 'checkbox', desc: 'Enabled?' },
-		{ name: '4', type: 'text', desc: 'Your name please' },
-		{ name: '5', type: 'text', desc: 'Your jacket' },
-	], desc: 'My character description (this is an object type)' },
-	{ name: '4', desc: 'Try on some sushi', options: [
-		{ name: 'fresh', desc: 'Fresh sushi' },
-		{ name: 'roll', desc: 'Choose a type', type: 'select',
-			options: [
-				'Tuna Roll', 'Thumbtack Roll', 'Hairball Roll', 'Flem Roll'] },
-		{ name: 'color', type: 'color', desc: 'Choose a color fish' },
-	] },
-	{ type: 'h3', desc: 'My List' },
-	{ type: 'list', name: 'mylist', desc: 'Here\'s a list, add some things',
-		head: true, sortable: true, fields: [
-			{ type: 'select', name: 'number', desc: 'Number',
-				options: ['One', 'Two'] },
-			{ type: 'text', name: 'hello', desc: 'Name' },
-			{ type: 'checkbox', name: 'yes', desc: 'Yeah?' },
-	] }
+  { name: '2', desc: 'Enable watching' },
+  { name: '3', desc: 'Allow users to start it out' },
+  { type: 'h3', desc: 'Section' },
+  { type: 'row', options: [
+    { name: 'first', type: 'text', desc: 'First Name' },
+    { name: 'made-up', type: 'text', desc: 'Nick Name' },
+    { name: 'name3', type: 'text', desc: 'Work Name' },
+  ] },
+  { name: '4', type: 'object', options: [
+    { name: 'enabled', type: 'checkbox', desc: 'Enabled?' },
+    { name: '5', type: 'text', desc: 'Your jacket' },
+  ], desc: 'My character description (this is an object type)' },
+  { name: '4', desc: 'Enable the sushi feature', options: [
+    { name: 'fresh', desc: 'Fresh sushi' },
+    { name: 'roll', desc: 'Choose a type', type: 'select',
+      options: [
+        'Tuna Roll', 'Thumbtack Roll', 'Hairball Roll', 'Eel Roll'] },
+    { name: 'color', type: 'color', desc: 'Choose a color fish' },
+  ] },
+  { type: 'h3', desc: 'My Todo List' },
+  { type: 'list', name: 'mylist', desc: 'Here\'s a list, add some things',
+    head: true, sortable: true, fields: [
+      { type: 'select', name: 'type', desc: 'Type',
+        options: ['Personal', 'Work', 'Study'] },
+      { type: 'text', name: 'desc', desc: 'Todo' },
+      { type: 'checkbox', name: 'done', desc: 'Done?' },
+    ] }
+]);
+
+chrome.options.addTab('Advanced', [
+  { type: 'predefined_sound', name: 'psound',
+    desc: 'Choose from a list of common alert sounds' },
+  { type: 'custom_sound', name: 'csound',
+    desc: 'Or provide your own' },
 ]);
